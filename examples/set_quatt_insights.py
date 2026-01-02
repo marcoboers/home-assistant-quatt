e1df9dc9457c8a2bcb48f3a58a816a30a1751f71
Patrick Vorgers <patrick@vorgers-stolle.nl>
2026-01-02 18:35:18 +0100

    Added other timeframe example Apex charts and removed the need to define helper sensors. (#276)

--- examples/set_quatt_insights.py
+++ examples/set_quatt_insights.py
@@ -1,7 +1,9 @@
 entity_id = data.get("entity_id", "sensor.quatt_insights")
 insights = data.get("insights")
+new_state = data.get("new_state", "ok")
+
 # Check whether insights have been received
 if not insights:
     logger.warning("set_quatt_insights: no insights data provided")
 else:
-    hass.states.set(entity_id, "ok", {"insights_json": insights})
+    hass.states.set(entity_id, new_state, {"insights_json": insights})
